a72dc13cc8bbc91c8c0307781188f2e7da6f5d90
Станіслав Шелудько <16stas.sheludko16@gmail.com>
2023-08-07 16:20:26 +0300

    sec commit

--- main.go
+++ main.go
@@ -1,20 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func calculateHandler(w http.ResponseWriter, r *http.Request) {
-	result := 10 + 5
-	fmt.Fprintf(w, "Result: %d", result)
+type InputData struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+type OutputData struct {
+	ResultA int `json:"a"`
+	ResultB int `json:"b"`
+}
+
+func calculateFactorial(n int, ch chan<- int) {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	ch <- result
+}
+
+func calculateHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	var inputData InputData
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&inputData); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	if inputData.A < 0 || inputData.B < 0 {
+		http.Error(w, `{"error":"Incorrect input"}`, http.StatusBadRequest)
+		return
+	}
+
+	resultChA := make(chan int)
+	resultChB := make(chan int)
+	go calculateFactorial(inputData.A, resultChA)
+	go calculateFactorial(inputData.B, resultChB)
+
+	resultA, resultB := <-resultChA, <-resultChB
+
+	outData := OutputData{
+		ResultA: resultA,
+		ResultB: resultB,
+	}
+
+	resultJSON, err := json.Marshal(outData)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resultJSON)
 }
 
 func main() {
-	http.HandleFunc("/calculate", calculateHandler)
+	router := httprouter.New()
+	router.POST("/calculate", calculateHandler)
 
 	port := 8989
-	fmt.Printf("Server working on %d port...\n", port)
 
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("Server working on %d port ...\n", port)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
